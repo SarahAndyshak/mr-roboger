e712e2ea6bbb206cfbbb46f60625d800e997afe2
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-28 14:55:08 -0800

    Update display method so that a new array is displayed each time, rather than added onto existing array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 
 function beepBoop(n) {
-  let outputArray = [];
+  let outputArray = []; //not actually needed if first loop is omitted
   let finalArray = [];
   for (let i = 0; i <= n; i++) {
     if (i === 3) {
@@ -13,7 +13,7 @@ function beepBoop(n) {
     } else { 
       outputArray.push(i)
     }
-  }
+  } //note lines 18 and 26 don't need brackets around i, change this to make lines 6-16 unnecessary
   for (let i = 0; i <= n; i++) {
     let strings = outputArray[i].toString(); //create a variable to store string
       if (strings.includes("3")) {
@@ -35,7 +35,7 @@ function handleFormSubmission(event) {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value; //assign a variable to the inputted number
   const finalArr = beepBoop(passage); //reading the inputted number and running the function to return finalArray
-  document.querySelector("div#add-array").append(finalArr); //should attach finalArray stored in finalArr
+  document.querySelector("div#add-array").innerText = finalArr; //change the way the array is displayed so that it replaces each time a new number is entered.
 }
 
 window.addEventListener("load", function() {
